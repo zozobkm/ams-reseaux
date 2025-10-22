4e23efcf185d692100e43dd4cacd985c04fe5ffd
belkacem matallah zohra <zmatallah42@gmail.com>
2025-10-22 09:35:30 +0200

    Refactor DHCP configuration script for automation

--- scripts/config_dhcp_auto.sh
+++ scripts/config_dhcp_auto.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-NB_APPAREILS=$1
+RESEAU=$1
+MASQUE=$2
+DEBUT=$3
+FIN=$4
+PASSERELLE=$5
 
-# Détection automatique de l'interface réseau interne
-INTERFACE=$(ip -o link show | awk -F': ' '{print $2}' | grep -E '^eth1|enp0s8' | head -n1)
-if [ -z "$INTERFACE" ]; then
-  echo "Erreur : impossible de détecter l'interface réseau interne."
-  exit 1
-fi
-
-
-RESEAU="192.168.10.0"
-MASQUE="255.255.255.0"
-DEBUT="192.168.10.10"
-FIN="192.168.10.$((10+NB_APPAREILS))"
-PASSERELLE="192.168.10.1"
-
-echo "Interface utilisée : $INTERFACE"
-echo "Configuration du DHCP pour $NB_APPAREILS appareils..."
-
+echo "Configuration DHCP sur le réseau $RESEAU ..."
 sudo bash -c "cat > /etc/dhcp/dhcpd.conf" <<EOF
 default-lease-time 600;
 max-lease-time 7200;
@@ -29,9 +17,6 @@ subnet $RESEAU netmask $MASQUE {
 }
 EOF
 
-# On redémarre le service DHCP
-sudo sed -i "s/INTERFACESv4=.*/INTERFACESv4=\"$INTERFACE\"/" /etc/default/isc-dhcp-server
+sudo sed -i "s/INTERFACESv4=.*/INTERFACESv4=\"eth1\"/" /etc/default/isc-dhcp-server
 sudo systemctl restart isc-dhcp-server
-
-echo "DHCP configuré réussite"
-sudo systemctl status isc-dhcp-server --no-pager
+echo " DHCP configuré automatiquement !"
