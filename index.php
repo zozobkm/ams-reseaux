727e0b6d59072e8ba02448bd7d4520f6368d56be
belkacem matallah zohra <zmatallah42@gmail.com>
2025-10-22 09:16:59 +0200

    Ajout de la page d'accueil du portail AMS

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>AMS - Portail Réseaux</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h1>Bienvenue sur votre Box Réseaux</h1>
+<nav>
+ <a href="templates/dns.php">DNS</a> |
+ <a href="templates/dhcp.php">DHCP</a> |
+ <a href="templates/ftp.php">FTP</a> |
+ <a href="templates/mail.php">Mail</a>
+</nav>
+<p>Choisissez un service à configurer.</p>
+</body>
+</html>
