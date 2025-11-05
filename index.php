a0995c5b0cf31f99374f281c50f5c90d45b6f2e7
belkacem matallah zohra <zmatallah42@gmail.com>
2025-11-05 10:09:46 +0100

    Change index.php to redirect to ip_form.php
    Redirect to ip_form.php instead of displaying HTML.

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-<meta charset="UTF-8">
-<title>AMS - Portail Réseaux</title>
-<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-<h1>Bienvenue sur votre Box Réseaux</h1>
-<nav>
- <a href="templates/ip_edit.php">Modif interfaces</a>
- <a href="templates/dns.php">DNS</a>
- <a href="templates/ip_form.php">DHCP eth1</a> 
- <a href="templates/dhcp.php">DHCP </a> 
- 
-
- 
-</nav>
-<p>Choisissez un service à configurer.</p>
-</body>
-</html>
+<?php
+header("Location: templates/ip_form.php");
+exit;
+?>
