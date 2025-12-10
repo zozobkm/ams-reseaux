fb6427648725f9cf60badc52d1c67da45f9004af
belkacem matallah zohra <zmatallah42@gmail.com>
2025-12-10 09:04:34 +0100

    Update dns.php

--- templates/dns.php
+++ templates/dns.php
@@ -1,24 +1,34 @@
-
 <!DOCTYPE html>
 <html lang="fr">
-<head><meta charset="UTF-8"><title>Configuration DNS</title></head>
- <?php include('menu.php'); ?>
+<head>
+    <meta charset="UTF-8">
+    <title>Configuration DNS</title>
+</head>
+
+<?php include('menu.php'); ?>
 
 <body>
 <h2>Configurer un domaine DNS</h2>
+
 <form method="post">
-<label>Domaine :</label>
-<input type="text" name="domaine" required>
-<input type="submit" name="configurer" value="Configurer">
+    <label>Domaine :</label>
+    <input type="text" name="domaine" placeholder="ex : zozo" required>
+    <input type="submit" name="configurer" value="Configurer">
 </form>
 
 <?php
 if(isset($_POST['configurer'])){
- $domaine=$_POST['domaine'];
- echo "<pre>";
- echo shell_exec("sudo bash ../scripts/config_dns.sh $domaine 2>&1");
- echo "</pre>";
+    $domaine = trim($_POST['domaine']);
+
+    if($domaine !== ""){
+        echo "<h3>Résultat :</h3><pre>";
+        echo shell_exec("sudo bash /var/www/html/ams-reseaux/scripts/config_dns.sh $domaine 2>&1");
+        echo "</pre>";
+    } else {
+        echo "<p style='color:red;'>Erreur : domaine vide.</p>";
+    }
 }
 ?>
+
 </body>
 </html>
