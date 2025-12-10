2dbd0276439208464e1b67f008caa09a7b5b71ed
belkacem matallah zohra <zmatallah42@gmail.com>
2025-12-10 09:52:05 +0100

    Add NAT activation form and execution script

--- templates/nat.php
+++ templates/nat.php
@@ -1,1 +1,18 @@
 
+<?php include('menu.php'); ?>
+
+<h2>Activation du NAT</h2>
+
+<form method="post">
+    <button type="submit" name="activer">Activer le NAT</button>
+</form>
+
+<?php
+if (isset($_POST['activer'])) {
+
+    $cmd = "sudo /var/www/html/ams-reseaux/scripts/config_nat.sh";
+    $resultat = shell_exec("$cmd 2>&1");
+
+    echo "<hr><pre>$resultat</pre>";
+}
+?>
