f610ad9ed87cbb62e13ed36841400594aef8e8bc
belkacem matallah zohra <zmatallah42@gmail.com>
2025-12-15 14:25:14 +0100

    Update delete.php

--- forum/delete.php
+++ forum/delete.php
@@ -1,12 +1,19 @@
 <?php
+session_start();
 require_once 'db.php';
+
+if (!isset($_SESSION['admin'])) {
+    die("Accès refusé");
+}
 
 if (!isset($_POST['id'])) {
     die("ID manquant");
 }
 
+$id = intval($_POST['id']);
+
 $stmt = $pdo->prepare("DELETE FROM messages WHERE id = ?");
-$stmt->execute([$_POST['id']]);
+$stmt->execute([$id]);
 
 header("Location: index.php");
 exit;
