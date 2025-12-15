d5f00c3caffd0dc5cc79d74e1968678b7c81a169
belkacem matallah zohra <zmatallah42@gmail.com>
2025-12-15 17:10:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(isset($_SESSION["user_id"])){
-    header("Location: /ams-reseaux/dashboard/index.php");
+    header("Location: /ams-reseaux/dahboard/index.php");
 }else{
     header("Location: /ams-reseaux/auth/login.php");
 }
