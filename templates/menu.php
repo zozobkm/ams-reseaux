72fd5e9e3aeb304552502c50b7c7e1867e055705
belkacem matallah zohra <zmatallah42@gmail.com>
2025-12-15 13:40:50 +0100

    Update menu.php

--- templates/menu.php
+++ templates/menu.php
@@ -1,31 +1,4 @@
-<style>
-nav{
-    background:#0078d7;
-    color:white;
-    padding:12px;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    box-shadow:0 2px 4px rgba(0,0,0,0.2);
-    position:fixed;
-    top:0;
-    left:0;
-    width:100%;
-    z-index:1000;
-}
-body{padding-top:60px;}
 
-nav a{
-    color:white;
-    text-decoration:none;
-    margin:0 20px;
-    font-weight:bold;
-    transition:0.3s;
-}
-nav a:hover{
-    text-decoration:underline;
-}
-</style>
 
 <nav>
     <a href="/ams-reseaux/index.php">Accueil</a>
