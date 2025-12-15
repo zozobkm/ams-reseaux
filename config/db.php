5d7b027e2431ad84d22366a7d0285a2303c16274
belkacem matallah zohra <zmatallah42@gmail.com>
2025-12-15 16:15:39 +0100

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$dbname = "box";       // IMPORTANT : base box pour login
+$user = "forumuser";
+$pass = "forum123";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} catch (PDOException $e) {
+    die("Erreur DB BOX : " . $e->getMessage());
+}
