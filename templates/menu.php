ee6de3d91cd5ba48ef027293d3ddff3fde3ce98e
belkacem matallah zohra <zmatallah42@gmail.com>
2025-12-15 13:30:40 +0100

    Add Forum link to the menu

--- templates/menu.php
+++ templates/menu.php
@@ -33,4 +33,6 @@ nav a:hover{
     <a href="/ams-reseaux/templates/dhcp.php">DHCP</a>
     <a href="/ams-reseaux/templates/dns.php">DNS</a>
     <a href="/ams-reseaux/templates/nat.php">Nat</a>
+    <a href="/ams-reseaux/forum/index.php">Forum</a>
+
 </nav>
