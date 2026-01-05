1bf8289f735d43fdab549431072c897584f14c1f
belkacem matallah zohra <zmatallah42@gmail.com>
2026-01-05 11:11:48 +0100

    Refactor database connection configuration

--- config.php
+++ config.php
@@ -1,12 +1,15 @@
 <?php
-$DB_HOST="localhost";
-$DB_NAME="box";
-$DB_USER="forumuser";
-$DB_PASS="forum123";
+// Configuration globale de la base de données
+$DB_HOST = "localhost";
+$DB_NAME = "box";
+$DB_USER = "forumuser";
+$DB_PASS = "forum123";
 
-try{
-    $pdo_box=new PDO("mysql:host=".$DB_HOST.";dbname=".$DB_NAME.";charset=utf8",$DB_USER,$DB_PASS);
-    $pdo_box->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-}catch(Exception $e){
-    die("Erreur DB BOX: ".$e->getMessage());
+try {
+    // On utilise une variable unique $pdo_box pour tout le projet
+    $pdo_box = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8", $DB_USER, $DB_PASS);
+    $pdo_box->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die("Erreur critique de connexion : " . $e->getMessage());
 }
+?>
