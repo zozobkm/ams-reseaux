dbd88c1123d3643e303d2a1a0875d6bd8a2b723c
belkacem matallah zohra <zmatallah42@gmail.com>
2026-01-06 20:56:25 +0100

    Update config_mail.sh

--- scripts/config_mail.sh
+++ scripts/config_mail.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-# Script de création de compte mail local
-if [ "$1" == "add" ]; then
-    useradd -m -s /usr/sbin/nologin "$2"
-    echo "$2:password123" | chpasswd
-    echo "Utilisateur $2 créé avec succès."
+# Usage: sudo ./config_mail.sh add [username] [password]
+
+ACTION=$1
+USER_NAME=$2
+USER_PASS=$3
+
+if [ "$ACTION" == "add" ]; then
+    # Verification si l'utilisateur existe deja
+    if id "$USER_NAME" &>/dev/null; then
+        echo "Erreur : L'utilisateur $USER_NAME existe deja."
+        exit 1
+    fi
+
+    # Creation du compte sans acces au shell pour la securite
+    sudo useradd -m -s /usr/sbin/nologin "$USER_NAME"
+    
+    # Definition du mot de passe passe en argument
+    echo "$USER_NAME:$USER_PASS" | sudo chpasswd
+    
+    echo "Compte mail pour $USER_NAME cree avec succes sur le domaine ceri.com."
 fi
