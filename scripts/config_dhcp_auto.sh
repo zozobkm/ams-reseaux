498211c95d23009ec41d2769f122334638935f9f
belkacem matallah zohra <zmatallah42@gmail.com>
2026-01-06 20:43:19 +0100

    Update config_dhcp_auto.sh

--- scripts/config_dhcp_auto.sh
+++ scripts/config_dhcp_auto.sh
@@ -6,17 +6,22 @@ FIN=$4
 PASSERELLE=$5
 
 echo "Configuration DHCP sur le réseau $RESEAU ..."
+
 sudo bash -c "cat > /etc/dhcp/dhcpd.conf" <<EOF
 default-lease-time 600;
 max-lease-time 7200;
 authoritative;
+
 subnet $RESEAU netmask $MASQUE {
   range $DEBUT $FIN;
   option routers $PASSERELLE;
-  option domain-name-servers 8.8.8.8;
+  option domain-name-servers $PASSERELLE; # Les clients interrogent ta CeriBox
+  option domain-name "ceri.com";
 }
 EOF
 
-sudo sed -i "s/INTERFACESv4=.*/INTERFACESv4=\"eth1\"/" /etc/default/isc-dhcp-server
+# On cible eth0 qui est ton interface LAN
+sudo sed -i 's/INTERFACESv4=.*/INTERFACESv4="eth0"/' /etc/default/isc-dhcp-server
+
 sudo systemctl restart isc-dhcp-server
-echo " DHCP configuré automatiquement !"
+echo "DHCP configuré sur eth0 pour le domaine ceri.com !"
