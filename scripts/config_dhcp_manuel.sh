c0f87cd7d47b0fdcb2e237f52233fd54fa32c21f
belkacem matallah zohra <zmatallah42@gmail.com>
2026-01-07 09:44:17 +0100

    Update config_dhcp_manuel.sh

--- scripts/config_dhcp_manuel.sh
+++ scripts/config_dhcp_manuel.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
-RESEAU=$1
-MASQUE=$2
-DEBUT=$3
-FIN=$4
-PASSERELLE=$5
 
-echo " Configuration du DHCP..."
+# On ignore le premier argument ($1 = "manuel")
+# On récupère les adresses IP envoyées par le PHP
+IP_DEBUT=$2
+IP_FIN=$3
+
+# Variables fixes pour la CeriBox
+RESEAU="192.168.1.0"
+MASQUE="255.255.255.0"
+PASSERELLE="192.168.1.1"
+
+echo "Configuration du DHCP pour le réseau $RESEAU..."
+
+# Écriture sécurisée du fichier
 sudo bash -c "cat > /etc/dhcp/dhcpd.conf" <<EOF
 default-lease-time 600;
 max-lease-time 7200;
 authoritative;
+
 subnet $RESEAU netmask $MASQUE {
-  range $DEBUT $FIN;
+  range $IP_DEBUT $IP_FIN;
   option routers $PASSERELLE;
-  option domain-name-servers 8.8.8.8;
+  option domain-name-servers $PASSERELLE;
 }
 EOF
 
-# On active le service sur eth1
+# Configuration de l'interface d'écoute
 sudo sed -i "s/INTERFACESv4=.*/INTERFACESv4=\"eth1\"/" /etc/default/isc-dhcp-server
 
-# Redémarrage
+# Redémarrage du service
 sudo systemctl restart isc-dhcp-server
-echo "DHCP configuré  $RESEAU"
+
+# Affichage du statut pour les logs de l'interface Web
+echo "DHCP configuré avec succès."
 sudo systemctl status isc-dhcp-server --no-pager | head -n 6
