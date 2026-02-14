d45ec945d49dfc0b29b18e11d2cee56a8e86af2f
belkacem matallah zohra <zmatallah42@gmail.com>
2026-02-15 00:15:10 +0100

    Update dns.php

--- services/dns.php
+++ services/dns.php
@@ -15,6 +15,7 @@ if (isset($_POST['configurer'])) {
     // Exécution de la commande DNS via le script Bash
     $cmd = "sudo bash /var/www/html/ams-reseaux/scripts/config_dns.sh $domaine";
     $resultat = shell_exec($cmd . " 2>&1");
+    echo "<pre>$resultat</pre>";
 }
 ?>
 <!DOCTYPE html>
