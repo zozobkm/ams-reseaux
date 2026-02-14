5c880671be293c86cb36d9f133d1425e76016da5
belkacem matallah zohra <zmatallah42@gmail.com>
2026-02-14 22:13:24 +0100

    Fix IP range calculation and update echo statement
    Updated DHCP configuration script to handle IP range correctly.

--- scripts/config_dhcp_auto.sh
+++ scripts/config_dhcp_auto.sh
@@ -3,7 +3,7 @@
 # Récupération du nombre d'appareils envoyé par le PHP
 NB=$1
 DEBUT_IP=10
-# Calcul de la fin de plage (ex: si 10 appareils, finit à .19)
+# Calcul de la fin de plage
 FIN_IP=$((DEBUT_IP + NB - 1))
 
 # Sécurité pour ne pas dépasser la limite du réseau
@@ -11,7 +11,8 @@ if [ $FIN_IP -gt 254 ]; then FIN_IP=254; fi
 
 echo "Configuration Auto pour $NB appareils..."
 
-sudo bash -c "cat > /etc/dhcp/dhcpd.conf" <<EOF
+# On écrit directement dans le fichier (le script sera lancé en sudo par le PHP)
+cat > /etc/dhcp/dhcpd.conf <<EOF
 default-lease-time 600;
 max-lease-time 7200;
 authoritative;
@@ -24,5 +25,6 @@ subnet 192.168.10.0 netmask 255.255.255.0 {
 }
 EOF
 
-sudo systemctl restart isc-dhcp-server
-echo "DHCP Auto configuré : 192.168.1.$DEBUT_IP à 192.168.1.$FIN_IP"
+# Redémarrage du service
+systemctl restart isc-dhcp-server
+echo "DHCP Auto configuré : 192.168.10.$DEBUT_IP à 192.168.10.$FIN_IP"
