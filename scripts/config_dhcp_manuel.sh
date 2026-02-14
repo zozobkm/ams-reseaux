274bcb0d110e530d5e6187b02c62c3ad3d65c784
belkacem matallah zohra <zmatallah42@gmail.com>
2026-02-14 22:12:23 +0100

    Update DHCP configuration for 10.x network

--- scripts/config_dhcp_manuel.sh
+++ scripts/config_dhcp_manuel.sh
@@ -3,15 +3,15 @@
 # Récupération du nombre d'appareils
 NB=$1
 
-# Calcul de la plage : on commence à .10
-DEBUT="192.168.1.10"
-FIN_OCTET=$((10 + NB))
-FIN="192.168.1.$FIN_OCTET"
+# Calcul de la plage : on commence à .10 sur le réseau 10.x
+DEBUT="192.168.10.10"
+FIN_OCTET=$((10 + NB - 1)) # -1 pour avoir exactement le nombre demandé
+FIN="192.168.10.$FIN_OCTET"
 
-# Paramètres fixes
-RESEAU="192.168.1.0"
+# Paramètres fixes alignés sur eth1 (192.168.10.1)
+RESEAU="192.168.10.0"
 MASQUE="255.255.255.0"
-PASSERELLE="192.168.1.1"
+PASSERELLE="192.168.10.1"
 
 sudo bash -c "cat > /etc/dhcp/dhcpd.conf" <<EOF
 default-lease-time 600;
