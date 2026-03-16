c4bcc673aa9520c04420a26af87652d720b61baf
belkacem matallah zohra <zmatallah42@gmail.com>
2026-03-16 15:19:28 +0100

    Update generate_test_file.php

--- services/generate_test_file.php
+++ services/generate_test_file.php
@@ -1,5 +1,6 @@
 <?php
-// Simule un fichier de 10Mo pour tester la bande passante
+// Génère 10 Mo de données pour simuler un vrai transfert
 header("Content-Type: application/octet-stream");
+header("Content-Length: " . (10 * 1024 * 1024));
 echo str_repeat("0", 10 * 1024 * 1024);
 ?>
