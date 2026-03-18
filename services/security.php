9ff47073611b40e203d0509dceb8c8117b0b3ca3
belkacem matallah zohra <zmatallah42@gmail.com>
2026-03-18 09:36:14 +0100

    Update security.php

--- services/security.php
+++ services/security.php
@@ -2,13 +2,11 @@
 // services/security.php
 
 function filtrer_censure($texte) {
-    // Ta liste de mots interdits (Tâche S6)
-    $mots_interdits = ["hack", "virus", "crack", "password", "root", "prout"];
-    
-    // Le remplacement "pro"
+    // Ta liste de mots interdits 
+    $mots_interdits = ["hack", "virus", "crack", "password", "root"];
+  
     $remplacement = "<span class='censored' style='color:red; font-weight:bold;'>[CENSURÉ]</span>";
     
-    // On filtre sans tenir compte de la casse (HACK = hack)
     return str_ireplace($mots_interdits, $remplacement, $texte);
 }
 ?>
