9e1934a5303fdaf3f6cc7ef5b652442a868af3c6
belkacem matallah zohra <zmatallah42@gmail.com>
2026-04-02 15:31:27 +0200

    Update update_blacklist.sh

--- scripts/update_blacklist.sh
+++ scripts/update_blacklist.sh
@@ -2,12 +2,17 @@
 echo "1. Téléchargement de la Blacklist (StevenBlack)..."
 wget -qO /tmp/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 
+# CRÉATION DU FICHIER BLACKHOLE MANQUANT !
+cat > /etc/bind/db.blackhole <<EOF
+\$TTL    86400
+@       IN      SOA     localhost. root.localhost. ( 1 604800 86400 2419200 86400 )
+@       IN      NS      localhost.
+@       IN      A       0.0.0.0
+EOF
+
 echo "2. Conversion pour Bind9..."
-# On prend les lignes qui commencent par 0.0.0.0, on enlève l'IP, et on formate en "zone" Bind9
-# On ignore localhost et on évite de bloquer la machine elle-même.
 grep "^0\.0\.0\.0" /tmp/hosts | grep -v "0.0.0.0 0.0.0.0" | awk '{print "zone \""$2"\" { type master; file \"/etc/bind/db.blackhole\"; };"}' > /etc/bind/named.conf.blacklist
 
 echo "3. Application des nouvelles règles..."
-systemctl reload bind9
-
+systemctl restart bind9
 echo "Mise à jour terminée avec succès !"
