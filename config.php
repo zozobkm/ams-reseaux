b1acc569594d36322fd3911b44585a703d43a285
belkacem matallah zohra <zmatallah42@gmail.com>
2026-04-28 19:31:05 +0200

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $DB_USER = "forumuser";
 $DB_PASS = "forum123";
 
 try {
-    // On utilise une variable unique $pdo_box pour tout le projet
+   
     $pdo_box = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8", $DB_USER, $DB_PASS);
     $pdo_box->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
