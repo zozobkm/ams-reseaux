5593c70a50f057f8fb398ee0ef1fb22d037a5e55
belkacem matallah zohra <zmatallah42@gmail.com>
2026-04-28 19:22:38 +0200

    Update db.php

--- services/db.php
+++ services/db.php
@@ -2,8 +2,5 @@
 // On utilise le chemin relatif pour atteindre config.php à la racine
 require_once __DIR__ . "/../config.php";
 
-/** * On crée l'alias $pdo pour ne pas avoir à modifier 
- * tes autres fichiers (forum.php, post.php, etc.)
- */
 $pdo = $pdo_box; 
 ?>
